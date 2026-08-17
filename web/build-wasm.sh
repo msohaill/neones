492e348c42b628336cfb5bfaf08310c5db02bf7f
Muhammad Sohail <mhsohail56@gmail.com>
2026-08-16 20:13:34 -0400

    New build

--- web/build-wasm.sh
+++ web/build-wasm.sh
@@ -5,12 +5,18 @@ then
   echo "Installing Rustup..."
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
   . "$HOME/.cargo/env"
+  echo "Rustup installed successfully."
 fi
 
 if ! command -v wasm-pack &> /dev/null
 then
   echo "Installing wasm-pack..."
-  curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+  curl https://wasm-bindgen.github.io/wasm-pack/installer/init.sh -sSf | sh
+  echo "wasm-pack installed successfully."
 fi
 
+rustup --version
+wasm-pack --version
+
+rustup target add wasm32-unknown-unknown
 wasm-pack build ./neones-web --target web
